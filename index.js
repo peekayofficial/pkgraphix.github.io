1ce007aba969bf119975077cf1fa396ea6490274
tinotendamukudu <mukudutinotenda@gmail.com>
2023-10-31 09:20:24 +0200

    new addittions

---
+++ index.js
@@ -0,0 +1,22 @@
+function sendMail(){
+    var params = {
+        name: document.getElementById("name").value ,
+        email: document.getElementById("email").value ,
+        message: document.getElementById("message").value ,
+
+    };
+    const serviceID = "service_b61hjqm";
+    const templateID = "template_egegq0l";
+
+    emailjs
+        .send(serviceID, templateID, params)
+        .then((res) => {
+            document.getElementById("name").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("message").value = "";
+            console.log(res);
+            alert("your message send successfully")
+        })
+        .catch((err) => console.log(err));
+}
+
